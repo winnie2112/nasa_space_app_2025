f56eb21c426e2d1c42a11ae88b0b1cd7fa41d596
Raimon Gramlich <uzidy@student.kit.edu>
2025-10-05 03:07:42 +0200

    Adjust description method - changed params order - allows partial input

--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,8 @@
-def get_descriptions(rain, snowfall, wind_speed, temperature, cloud_cover):
+def get_descriptions(rain, temperature, snowfall=None, wind_speed=None, cloud_cover=None):
     # in mm
-    if rain < 1:
+    if rain == None:
+        rain_description = None
+    elif rain < 1:
         rain_description = "No Rain"
     elif rain >= 1 and rain < 30:
         rain_description = "Rainy"
@@ -8,7 +10,9 @@ def get_descriptions(rain, snowfall, wind_speed, temperature, cloud_cover):
         rain_description = "Heavy Rain"
 
     # in cm
-    if snowfall < 1:
+    if snowfall == None:
+        snow_description = None
+    elif snowfall < 1:
         snow_description = "No Snow"
     elif snowfall >= 1 and snowfall < 20:
         snow_description = "Snowy"
@@ -16,7 +20,9 @@ def get_descriptions(rain, snowfall, wind_speed, temperature, cloud_cover):
         snow_description = "Heavy Snow"
     
     # in km/h
-    if wind_speed < 1:
+    if wind_speed == None:
+        wind_description = None
+    elif wind_speed < 1:
         wind_description = "No Wind"
     elif wind_speed >= 1 and wind_speed <= 20:
         wind_description = "Gentle Breeze"
@@ -26,7 +32,9 @@ def get_descriptions(rain, snowfall, wind_speed, temperature, cloud_cover):
         wind_description = "Very Windy"
 
     # in °C
-    if temperature <= 0:
+    if temperature == None:
+        temperature_description = None
+    elif temperature <= 0:
         temperature_description = "Very Cold"
     elif temperature > 0 and temperature <= 10:
         temperature_description = "Cold"
@@ -38,7 +46,9 @@ def get_descriptions(rain, snowfall, wind_speed, temperature, cloud_cover):
         temperature_description = "Very Hot"
 
     # in %
-    if cloud_cover <= 30:
+    if cloud_cover == None:
+        cloud_description = None
+    elif cloud_cover <= 30:
         cloud_description = "Clear"
     elif cloud_cover > 30 and cloud_cover <= 70:
         cloud_description = "Partly Cloudy"
@@ -47,4 +57,4 @@ def get_descriptions(rain, snowfall, wind_speed, temperature, cloud_cover):
     else:
         cloud_description = "Overcast"
     
-    return rain_description, snow_description, wind_description, temperature_description, cloud_description
+    return rain_description, temperature_description,  snow_description, wind_description, cloud_description
